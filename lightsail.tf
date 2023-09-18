76fe283ff06199b0726ce0da532450ddfa625fe4
kizoka13 <kizoka13@gmail.com>
2023-09-18 16:17:29 -0400

    ticket number

---
+++ lightsail.tf
@@ -0,0 +1,18 @@
+resource "aws_lightsail_instance" "example_instance" {
+  name               = "example-instance"
+  availability_zone  = "us-east-1a"  
+  blueprint_id       = "amazon_linux_2"  
+  bundle_id          = "nano_2_0" 
+  #key_pair_name      = "your-key-pair-name"  
+  user_data          = <<-EOF
+    #!/bin/bash
+    sudo yum install httpd -y
+    sudo systemctl start httpd
+    sudo systemctl enable httpd
+    echo "<h1>This Server is created using Terraform</h1>" | sudo tee /var/www/html/index.html
+    EOF
+}
+
+output "instance_public_ip" {
+  value = aws_lightsail
+}
